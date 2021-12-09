defbf11157eeb5a963c6088b09444a7d13955f8c
Sujjada zabin <90233176+sujjada@users.noreply.github.com>
2021-12-09 13:06:01 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+//import 'package:weather_app/screens/home.dart';
+import 'package:weatherapp/screens/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home:HomePage(),
+   );
+  }
+}
